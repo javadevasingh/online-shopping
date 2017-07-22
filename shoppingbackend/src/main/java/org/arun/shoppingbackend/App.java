1f75fe1b2289ed5055e7567c714bd85dbc592273
Arun Singh <arunsinghbagal@gmail.com>
2017-07-23 00:19:25 +0530

    My Project Setup completed successfully

---
+++ shoppingbackend/src/main/java/org/arun/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package org.arun.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
